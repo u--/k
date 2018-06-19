4026b40fc8bf679ebd34670e8a3fcf24f5539290
Denis Bogdanas <denis.bogdanas@gmail.com>
2018-06-19 15:57:41 +0300

    kprove bugfix continued: unfeasible terms that contain constraints T and T ==K false: applied elimination algorithm in a few more places where constraint.isFalse() was used, but not all of them.

--- java-backend/src/main/java/org/kframework/backend/java/symbolic/SymbolicRewriter.java
+++ java-backend/src/main/java/org/kframework/backend/java/symbolic/SymbolicRewriter.java
@@ -209,8 +209,7 @@ public class SymbolicRewriter {
             if (!matchResult.isMatching) {
                 // TODO(AndreiS): move these some other place
                 result = result.expandPatterns(true);
-                if (result.constraint().isFalse() || result.constraint().isFalseFromContradictions()
-                        || result.constraint().checkUnsat()) {
+                if (result.constraint().isFalseExtended() || result.constraint().checkUnsat()) {
                     continue;
                 }
             }
@@ -396,7 +395,7 @@ public class SymbolicRewriter {
         if (expandPattern) {
             // TODO(AndreiS): move these some other place
             result = result.expandPatterns(true);
-            if (result.constraint().isFalse() || result.constraint().checkUnsat()) {
+            if (result.constraint().isFalseExtended() || result.constraint().checkUnsat()) {
                 result = null;
             }
         }
